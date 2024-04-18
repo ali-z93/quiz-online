7f44b128c644d3dd8e628a889c46a72382b7ee87
ali <ali@alis-mbp>
2024-04-18 15:44:03 -0400

    designing API

---
+++ src/main/java/com/aliz/quizonline/controller/QuestionController.java
@@ -0,0 +1,74 @@
+package com.aliz.quizonline.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.crossstore.ChangeSetPersister;
+import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.aliz.quizonline.model.Question;
+import com.aliz.quizonline.service.IQuestionService;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/quizzes")
+@RequiredArgsConstructor
+public class QuestionController {
+
+	private final IQuestionService questionservice;
+
+	@PostMapping("create-new-question")
+	public ResponseEntity<Question> createQuestion(@Valid @RequestBody Question question) {
+		Question createdQuestion = questionservice.createeQuestion(question);
+		return ResponseEntity.status(HttpStatus.CREATED).body(createdQuestion);
+	}
+
+	@GetMapping("/all-questions")
+	public ResponseEntity<List<Question>> getAllQuestions() {
+		List<Question> questions = questionservice.getAllQuestion();
+		return ResponseEntity.ok(questions);
+	}
+
+	@GetMapping("/question/{id}")
+	public ResponseEntity<Optional<Question>> getQuestionById(@PathVariable Long id) throws NotFoundException {
+		Optional<Question> theQuestion = questionservice.getQuestionById(id);
+		if (theQuestion.isPresent()) {
+			return ResponseEntity.ok(theQuestion);
+		} else {
+			throw new ChangeSetPersister.NotFoundException();
+		}
+
+	}
+
+	@PutMapping("/{id}/update")
+	public ResponseEntity<Question> update(@PathVariable Long id, @RequestBody Question question)
+			throws NotFoundException {
+		Question updatedQuestion = questionservice.updateQuestion(id, question);
+		return ResponseEntity.ok(updatedQuestion);
+	}
+
+	@DeleteMapping("{id}/delete")
+	public ResponseEntity<Void> deleteQuestion(@PathVariable Long id) {
+		questionservice.deleteQuestion(id);
+		return ResponseEntity.noContent().build();
+	}
+
+	@GetMapping("/subejcts")
+	public ResponseEntity<List<String>> getAllSubjects() {
+		List<String> subjects = questionservice.getAllSubjects();
+		return ResponseEntity.ok(subjects);
+	}
+
+}
